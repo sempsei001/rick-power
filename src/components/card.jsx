3dd74fa1a4cd66bd20a89384327ad28bbd4a555c
blitty-codes <blittynet@gmail.com>
2021-07-29 02:08:07 +0200

    Card for characters

---
+++ src/components/card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Card = ({ name, gender, status, image }) => {
+  return (
+    <div
+			className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 mb-1 lg:py-4 lg:flex lg:items-center lg:space-y-0 lg:space-x-6 lg:m-1.5">
+      <img
+        className="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0"
+        src={ image }
+        alt={`${name} image`}
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{ name }</p>
+          <p className="text-gray-500 font-medium">{ gender }</p>
+					<p className="text-gray-500 text-opacity-75 font-medium">{status === 'Alive' ? '🟢' : status === 'unknown' ? '❓' : '🔴'} { status }</p>
+        </div>
+        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          More information
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  name: PropTypes.string,
+	gender: PropTypes.string,
+	status: PropTypes.string,
+	image: PropTypes.string
+};
+
+export default Card;
